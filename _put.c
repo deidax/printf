b771f8048501a134874f778a1fe66b3a367121d7
deidax <khalildida93@gmail.com>
2023-06-27 06:00:34 -0700

    alx commit

---
+++ _put.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * put - Print a string and return the number of characters printed.
+ * @str: The input string to be printed.
+ *
+ * Description:
+ *     The `put` function prints the characters of the input string @str
+ *     and returns the number of characters printed. If @str is NULL,
+ *     it does not print anything and returns 0.
+ *
+ * Return:
+ *     The number of characters printed.
+ */
+int _put(char *str) {
+    int count = 0;
+
+    if (str != NULL) {
+        while (*str != '\0') {
+            putchar(*str);
+            str++;
+            count++;
+        }
+    }
+
+    return (count);
+}
--- main.h
+++ main.h
@@ -1,9 +1,16 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
+#ifndef MAIN_H
+#define MAIN_H
 #ifndef NULL
 #define NULL ((void *)0)
 #endif
 #include <stdio.h>
 #include <stdarg.h>
 #include <limits.h>
+#include <unistd.h>
+int _putchar(char c);
+int put(char *str);
+void print_char(va_list args);
+void print_string(va_list args);
+void print_percentage(va_list args);
+int _printf(const char *format, ...);
 #endif
